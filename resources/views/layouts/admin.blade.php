a06594cdd472d61c2011c4376d63089c120644c9
Dhicahya <leyasu45153@gmail.com>
2024-06-05 22:05:07 +0700

    build: admin pages

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Dashboard - NiceAdmin Bootstrap Template</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    @include('include.admin.style')
+
+</head>
+
+<body>
+    @include('include.admin.navbar')
+
+    @include('include.admin.sidebar')
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    @yield('content')
+
+    @include('include.admin.footer')
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+    
+    @include('include.admin.script')
+
+</body>
+
+</html>
